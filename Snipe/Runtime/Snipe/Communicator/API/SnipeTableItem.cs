e1e2b2480e76ebb704c195e4bd0bb30d5c8e2c23
gluks <gluk-02@yandex.ru>
2021-03-31 17:07:43 +0400

    SnipeTableAttrsItem

--- Snipe/Runtime/Snipe/Communicator/API/SnipeTableItem.cs
+++ Snipe/Runtime/Snipe/Communicator/API/SnipeTableItem.cs
@@ -1,4 +1,5 @@
 ﻿using MiniIT;
+using System;
 using System.Collections.Generic;
 
 namespace MiniIT.Snipe
@@ -11,5 +12,43 @@ namespace MiniIT.Snipe
 	public interface ISnipeTableItemsListWrapper<ItemType>
 	{
 		List<ItemType> list { get; set; }
+	}
+	
+	public class SnipeTableAttrsItem : SnipeTableItem
+	{
+		public Dictionary<string, object> attrs { get; set; }
+		
+		public T GetAttr<T>(string key) where T : new()
+		{
+			if (attrs == null)
+				return default;
+			
+			T result = default;
+			
+			if (attrs.TryGetValue(key, out var value))
+			{
+				try
+				{
+					result = (T)value;
+				}
+				catch (InvalidCastException)
+				{
+					try
+					{
+						result = (T)Convert.ChangeType(value, typeof(T));
+					}
+					catch (Exception)
+					{
+						result = default;
+					}
+				}
+				catch (NullReferenceException) // field exists but value is null
+				{
+					result = default;
+				}
+			}
+			
+			return result;
+		}
 	}
 }
