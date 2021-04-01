eabbc9c947a9e77e29700cd4a11c2c2d8a2955a2
gluks <gluk-02@yandex.ru>
2021-04-01 14:57:37 +0400

    Update SnipeTableItem.cs

--- Snipe/Runtime/Snipe/Communicator/API/SnipeTableItem.cs
+++ Snipe/Runtime/Snipe/Communicator/API/SnipeTableItem.cs
@@ -16,7 +16,7 @@ namespace MiniIT.Snipe
 	
 	public class SnipeTableAttrsItem : SnipeTableItem
 	{
-		public Dictionary<string, object> attrs { get; set; }
+		public object attrs { get; set; }
 		
 		public T GetAttr<T>(string key) where T : new()
 		{
@@ -24,25 +24,52 @@ namespace MiniIT.Snipe
 				return default;
 			
 			T result = default;
-			
-			if (attrs.TryGetValue(key, out var value))
+
+			if (attrs is IDictionary<string, object> attrs_dict)
 			{
-				try
-				{
-					result = (T)value;
-				}
-				catch (InvalidCastException)
+				if (attrs_dict.TryGetValue(key, out var value))
 				{
 					try
 					{
-						result = (T)Convert.ChangeType(value, typeof(T));
+						result = (T)value;
 					}
-					catch (Exception)
+					catch (InvalidCastException)
+					{
+						try
+						{
+							result = (T)Convert.ChangeType(value, typeof(T));
+						}
+						catch (Exception)
+						{
+							result = default;
+						}
+					}
+					catch (NullReferenceException) // field exists but value is null
 					{
 						result = default;
 					}
 				}
-				catch (NullReferenceException) // field exists but value is null
+			}
+			else
+			{
+				var attrs_type = attrs.GetType();
+				try
+				{
+					var field = attrs_type.GetField(key);
+					if (field != null)
+					{
+						result = (T)Convert.ChangeType(field.GetValue(attrs), typeof(T));
+					}
+					else
+					{
+						var prop = attrs_type.GetProperty(key);
+						if (prop != null)
+						{
+							result = (T)Convert.ChangeType(prop.GetValue(attrs), typeof(T));
+						}
+					}
+				}
+				catch (Exception)
 				{
 					result = default;
 				}
