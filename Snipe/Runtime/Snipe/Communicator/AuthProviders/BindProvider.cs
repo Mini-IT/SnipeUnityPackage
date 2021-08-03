d7b797cf4a1a2b9ca77d98b730728a7f2a14e068
gluks <gluk-02@yandex.ru>
2021-08-03 17:39:14 +0400

    Update BindProvider.cs

--- Snipe/Runtime/Snipe/Communicator/AuthProviders/BindProvider.cs
+++ Snipe/Runtime/Snipe/Communicator/AuthProviders/BindProvider.cs
@@ -97,7 +97,7 @@ namespace MiniIT.Snipe
 		{
 			DebugLogger.Log($"[BindProvider] ({ProviderId}) OnBindResponse - {error_code}");
 
-			if (error_code == "ok")
+			if (error_code == SnipeErrorCodes.OK)
 			{
 				AccountExists = true;
 				IsBindDone = true;
@@ -113,7 +113,7 @@ namespace MiniIT.Snipe
 			
 			bool is_me = data.SafeGetValue("isSame", false);
 			if (AccountExists == true && is_me)
-				IsBindDone = true;
+				IsBindDone = SnipeCommunicator.Instance.LoggedIn;
 
 			if (mCheckAuthExistsCallback != null)
 			{
@@ -121,7 +121,7 @@ namespace MiniIT.Snipe
 				mCheckAuthExistsCallback = null;
 			}
 
-			if (AccountExists.HasValue)
+			if (AccountExists.HasValue && SnipeCommunicator.Instance.LoggedIn)
 			{
 				if (AccountExists == false)
 				{
