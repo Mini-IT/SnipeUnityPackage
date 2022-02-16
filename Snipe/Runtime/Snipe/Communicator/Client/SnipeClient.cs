c3ad7527cb65eac8a6eb4fc6fde667b557d14029
gluks <gluk-02@yandex.ru>
2022-02-16 16:04:39 +0400

    SendTask fix

--- Snipe/Runtime/Snipe/Communicator/Client/SnipeClient.cs
+++ Snipe/Runtime/Snipe/Communicator/Client/SnipeClient.cs
@@ -304,7 +304,7 @@ namespace MiniIT.Snipe
 			mSendMessages = new ConcurrentQueue<SnipeObject>();
 
 			mSendTaskCancellation = new CancellationTokenSource();
-			_ = SendTask(mHeartbeatCancellation?.Token);
+			Task.Run(() => SendTask(mSendTaskCancellation?.Token));
 		}
 
 		private void StopSendTask()
