b8c991b840e7b7741dc1a65fe864d668598f8a14
gluks <gluk-02@yandex.ru>
2022-09-02 20:18:19 +0400

    Update DependenciesDetector.cs

--- Editor/DependenciesDetector.cs
+++ Editor/DependenciesDetector.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEditor.Compilation;
 using UnityEditor;
 
 using Debug = UnityEngine.Debug;
@@ -15,43 +17,88 @@ namespace MiniIT.Snipe.Editor
 	[InitializeOnLoad]
 	public static class DependenciesDetector
 	{
-		private const string NAMESPACE_FACEBOOK = "Facebook.Unity";
-		private const string DEFINE_FACEBOOK = "SNIPE_FACEBOOK";
+		private static readonly string[][] NAMESPACE_DEFINES = new []
+		{
+			new string[] { "Facebook.Unity", "SNIPE_FACEBOOK" },
+		};
 		
 		static DependenciesDetector()
         {
-			var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-            if (buildTargetGroup == BuildTargetGroup.Unknown)
-            {
-                var propertyInfo = typeof(EditorUserBuildSettings).GetProperty("activeBuildTargetGroup");//, BindingFlags.Static | BindingFlags.NonPublic);
-                if (propertyInfo != null)
-                    buildTargetGroup = (BuildTargetGroup)propertyInfo.GetValue(null, null);
-            }
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+				return;
 			
-			var previousProjectDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
-            var projectDefines = previousProjectDefines.Split(';').ToList();
+			Debug.Log($"[Snipe DependenciesDetector] start");
 			
-            RefreshDefineSymbolForNamespace(buildTargetGroup, projectDefines, NAMESPACE_FACEBOOK, DEFINE_FACEBOOK);
+			DelayedRun();
+		}
+		
+		private static async void DelayedRun()
+		{
+			await Task.Delay(200);
+			Debug.Log($"[Snipe DependenciesDetector] delay finished");
 			
-			PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, string.Join(";", projectDefines.ToArray()));
-
-			// Let other systems execute before reloading assemblies
-			Thread.Sleep(1000);
-			EditorApplication.UnlockReloadAssemblies();
+			//CompilationPipeline.compilationFinished += (context) =>
+            //{
+			//	Debug.Log($"[Snipe DependenciesDetector] compilationFinished");
+				
+				var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+				if (buildTargetGroup == BuildTargetGroup.Unknown)
+				{
+					var propertyInfo = typeof(EditorUserBuildSettings).GetProperty("activeBuildTargetGroup");//, BindingFlags.Static | BindingFlags.NonPublic);
+					if (propertyInfo != null)
+						buildTargetGroup = (BuildTargetGroup)propertyInfo.GetValue(null, null);
+				}
+				
+				Debug.Log($"[Snipe DependenciesDetector] buildTargetGroup = {buildTargetGroup}");
+				
+				EditorApplication.LockReloadAssemblies();
+				
+				var previousProjectDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+				var projectDefines = previousProjectDefines.Split(';').ToList();
+				
+				foreach (var item in NAMESPACE_DEFINES)
+				{
+					RefreshDefineSymbolForNamespace(buildTargetGroup, projectDefines, item[0], item[1]);
+				}
+				
+				var newProjectDefines = string.Join(";", projectDefines.ToArray());
+				if (newProjectDefines != previousProjectDefines)
+				{
+					Debug.Log($"[Snipe DependenciesDetector] define symbols changed - applying");
+					
+					//EditorApplication.LockReloadAssemblies();
+					
+					PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, newProjectDefines);
+					
+					// Let other systems execute before reloading assemblies
+					Thread.Sleep(1000);
+				}
+				
+				EditorApplication.UnlockReloadAssemblies();
+			//};
         }
 		
 		private static void RefreshDefineSymbolForNamespace(BuildTargetGroup buildTargetGroup, IList<string> projectDefines, string namespace_name, string define_symbol)
 		{
+			Debug.Log($"[Snipe DependenciesDetector] RefreshDefineSymbolForNamespace {buildTargetGroup} - {define_symbol} - {namespace_name}");
+			
 			if (EditorUtil.NamespaceExists(namespace_name))
 			{
+				Debug.Log($"[Snipe DependenciesDetector] -- namespace exists {namespace_name}");
 				if (!projectDefines.Contains(define_symbol, StringComparer.OrdinalIgnoreCase))
 				{
 					Debug.Log($"[Snipe DependenciesDetector] Add define symbol {define_symbol}");
 					projectDefines.Add(define_symbol);
 				}
+				else
+				{
+					Debug.Log($"[Snipe DependenciesDetector] -- define symbol exists {define_symbol}");
+				}
 			}
 			else
 			{
+				Debug.Log($"[Snipe DependenciesDetector] -- namespace does not exist {namespace_name}");
+				
 				if (projectDefines.Remove(define_symbol))
 				{
 					Debug.Log("[Snipe DependenciesDetector] Remove define symbol {define_symbol}");
