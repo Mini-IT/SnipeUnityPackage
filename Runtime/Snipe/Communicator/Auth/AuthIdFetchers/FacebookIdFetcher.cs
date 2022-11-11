0423caafe0629c68e29129b5280238ff7a2a6537
gluks <gluk-02@yandex.ru>
2022-11-11 18:53:44 +0600

    Update FacebookIdFetcher.cs

--- Runtime/Snipe/Communicator/Auth/AuthIdFetchers/FacebookIdFetcher.cs
+++ Runtime/Snipe/Communicator/Auth/AuthIdFetchers/FacebookIdFetcher.cs
@@ -2,15 +2,12 @@
 
 using System;
 using System.Threading.Tasks;
-using MiniIT.Social;
 using Facebook.Unity;
 
 namespace MiniIT.Snipe
 {
 	public class FacebookIdFetcher : AuthIdFetcher
 	{
-		private Action<string> mCallback;
-		
 		public override void Fetch(bool wait_initialization, Action<string> callback = null)
 		{
 			if (string.IsNullOrEmpty(Value))
@@ -23,39 +20,21 @@ namespace MiniIT.Snipe
 
 			if (wait_initialization && string.IsNullOrEmpty(Value))
 			{
-				Task.Run(() => WaitForInitialization(callback));
+				WaitForInitialization(callback);
 				return;
 			}
 
 			callback?.Invoke(Value);
 		}
 
-		private async Task WaitForInitialization(Action<string> callback)
+		private async void WaitForInitialization(Action<string> callback)
 		{
-			while (!FB.IsLoggedIn || AccessToken.CurrentAccessToken == null)
+			while (string.IsNullOrEmpty(AccessToken.CurrentAccessToken?.UserId))
 			{
 				await Task.Delay(100);
-				Value = AccessToken.CurrentAccessToken.UserId;
 			}
+			Value = AccessToken.CurrentAccessToken.UserId;
 			callback?.Invoke(Value);
-		}
-
-		private void OnFacebookProviderInitializationComplete()
-		{
-			FacebookProvider.InstanceInitializationComplete -= OnFacebookProviderInitializationComplete;
-
-			if (FB.IsLoggedIn && AccessToken.CurrentAccessToken != null)
-			{
-				Value = AccessToken.CurrentAccessToken.UserId;
-			}
-
-			InvokeCallback();
-		}
-
-		private void InvokeCallback()
-		{
-			mCallback?.Invoke(Value);
-			mCallback = null;
 		}
 	}
 }
