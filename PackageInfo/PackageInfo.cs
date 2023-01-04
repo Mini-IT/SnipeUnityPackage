36dfcce3b1687da278d63f4582074f9acfd9f7b0
gluks <gluk-02@yandex.ru>
2023-01-04 20:37:19 +0600

    Package version

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -2,6 +2,6 @@ namespace MiniIT.Snipe
 {
 	public static class PackageInfo
 	{
-		public static string VERSION = "3.2.6 client_refactoring";
+		public static string VERSION = "3.2.7 client_refactoring";
 	}
 }
