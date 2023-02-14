c91812b9df65fc535ab64f4d069bd1a2e52a9c34
gluks <gluk-02@yandex.ru>
2023-02-14 21:44:51 +0600

    Update AbstractSnipeApiService.cs

--- Runtime/Snipe/Communicator/API/AbstractSnipeApiService.cs
+++ Runtime/Snipe/Communicator/API/AbstractSnipeApiService.cs
@@ -1,8 +1,16 @@
 
+using System;
+using System.Threading.Tasks;
+
 namespace MiniIT.Snipe.Api
 {
-	public class AbstractSnipeApiService
+	public class AbstractSnipeApiService : IDisposable
 	{
+		protected internal AbstractSnipeApiService()
+		{
+			AttachCommunicator();
+		}
+
 		public SnipeCommunicatorRequest CreateRequest(string message_type, SnipeObject data)
 		{
 			if (SnipeCommunicator.Instance.LoggedIn || SnipeCommunicator.Instance.AllowRequestsToWaitForLogin)
@@ -11,6 +19,58 @@ namespace MiniIT.Snipe.Api
 			}
 			
 			return null;
+		}
+
+		public virtual void Dispose()
+		{
+			UnsubscribeCommunicatorEvents();
+		}
+
+		private async void AttachCommunicator()
+		{
+			// Allow the subclass constructor to finish
+			await Task.Yield();
+
+			while (!SnipeCommunicator.InstanceInitialized)
+			{
+				await Task.Delay(100);
+			}
+
+			if (SnipeCommunicator.Instance.Connected)
+			{
+				OnConnectionSucceeded();
+			}
+			else
+			{
+				SnipeCommunicator.Instance.ConnectionSucceeded += OnConnectionSucceeded;
+			}
+			SnipeCommunicator.Instance.PreDestroy += OnCommunicatorPreDestroy;
+
+			InitMergeableRequestTypes();
+		}
+
+		protected virtual void OnConnectionSucceeded()
+		{
+		}
+
+		protected virtual void InitMergeableRequestTypes()
+		{
+
+		}
+
+		protected virtual void OnCommunicatorPreDestroy()
+		{
+			UnsubscribeCommunicatorEvents();
+			AttachCommunicator();
+		}
+
+		private void UnsubscribeCommunicatorEvents()
+		{
+			if (SnipeCommunicator.InstanceInitialized)
+			{
+				SnipeCommunicator.Instance.ConnectionSucceeded -= OnConnectionSucceeded;
+				SnipeCommunicator.Instance.PreDestroy -= OnCommunicatorPreDestroy;
+			}
 		}
 	}
 }
