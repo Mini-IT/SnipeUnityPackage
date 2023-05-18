8583818ce726eb9f84818e1c6ed3e678f0d29948
gluks <gluk-02@yandex.ru>
2023-05-18 13:09:28 +0600

    LogicManager waits table loading

--- Runtime/Snipe/Communicator/API/Logic/LogicManager.cs
+++ Runtime/Snipe/Communicator/API/Logic/LogicManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace MiniIT.Snipe.Api
 {
@@ -18,6 +19,13 @@ namespace MiniIT.Snipe.Api
 		public event ExitNodeHandler ExitNode;
 		public event NodeProgressHandler NodeProgress;
 
+		class QueuedMessage
+		{
+			internal string messageType;
+			internal string errorCode;
+			internal SnipeObject data;
+		}
+
 		public Dictionary<int, LogicNode> Nodes { get; private set; } = new Dictionary<int, LogicNode>();
 		private Dictionary<string, LogicNode> _taggedNodes;
 		private SnipeTable<SnipeTableLogicItem> _logicTable = null;
@@ -30,16 +38,29 @@ namespace MiniIT.Snipe.Api
 		private Stopwatch _refTime = Stopwatch.StartNew();
 		private Timer _secondsTimer;
 
-		public LogicManager(SnipeCommunicator communicator, AbstractSnipeApiService.RequestFactoryMethod requestFactory, SnipeTable<SnipeTableLogicItem> logic_table)
+		private CancellationTokenSource _waitingTableCancellation;
+		private Queue<QueuedMessage> _waitingTableMessages;
+
+		public LogicManager(SnipeCommunicator communicator, AbstractSnipeApiService.RequestFactoryMethod requestFactory, SnipeTable<SnipeTableLogicItem> logicTable)
 		{
 			_requestFactory = requestFactory;
-			_logicTable = logic_table;
+			_logicTable = logicTable;
 
 			ClearCommunicatorReference();
 
 			_snipeCommunicator = communicator;
-			_snipeCommunicator.MessageReceived += OnSnipeMessageReceived;
-			_snipeCommunicator.PreDestroy += OnSnipeCommunicatorPreDestroy;
+
+			if (_logicTable != null)
+			{
+				_snipeCommunicator.MessageReceived += OnSnipeMessageReceived;
+				_snipeCommunicator.PreDestroy += OnSnipeCommunicatorPreDestroy;
+
+				if (!_logicTable.Loaded)
+				{
+					_waitingTableCancellation = new CancellationTokenSource();
+					WaitForTableLoaded(_waitingTableCancellation.Token);
+				}
+			}
 		}
 
 		~LogicManager()
@@ -54,6 +75,12 @@ namespace MiniIT.Snipe.Api
 
 		public void Dispose()
 		{
+			if (_waitingTableCancellation != null)
+			{
+				_waitingTableCancellation.Cancel();
+				_waitingTableCancellation = null;
+			}
+
 			StopSecondsTimer();
 
 			ClearCommunicatorReference();
@@ -157,15 +184,15 @@ namespace MiniIT.Snipe.Api
 
 		public void IncVar(string name, int tree_id = 0) //, int amount = 0)
 		{
-			SnipeObject request_data = new SnipeObject()
+			SnipeObject requestData = new SnipeObject()
 			{
 				["name"] = name,
 			};
 			if (tree_id != 0)
-				request_data["treeID"] = tree_id;
+				requestData["treeID"] = tree_id;
 			//if (amount != 0)
 			//	request_data["amount"] = amount;
-			var request = _requestFactory.Invoke("logic.incVar", request_data);
+			var request = _requestFactory.Invoke("logic.incVar", requestData);
 			if (request != null)
 			{
 				request.Request();
@@ -174,56 +201,67 @@ namespace MiniIT.Snipe.Api
 			_updateRequestedTime = TimeSpan.Zero; // reset timer
 		}
 
-		private void OnSnipeMessageReceived(string message_type, string error_code, SnipeObject data, int request_id)
+		private void OnSnipeMessageReceived(string messageType, string errorCode, SnipeObject data, int requestId)
 		{
-			switch (message_type)
+			switch (messageType)
 			{
 				case "logic.get":
-					OnLogicGet(error_code, data);
+					ProcessMessage(messageType, errorCode, data, OnLogicGet);
 					break;
 
 				case "logic.exitNode":
-					OnLogicExitNode(data);
+					ProcessMessage(messageType, errorCode, data, OnLogicExitNode);
 					break;
 
 				case "logic.progress":
-					OnLogicProgress(data);
+					ProcessMessage(messageType, errorCode, data, OnLogicProgress);
 					break;
 
 				case "logic.incVar":
-					RequestLogicGet(true);
+					ProcessMessage(messageType, errorCode, data, (errorCode, responseData) => RequestLogicGet(true));
 					break;
 			}
 		}
 
-		private void OnLogicGet(string error_code, SnipeObject response_data)
+		private void ProcessMessage(string messageType, string errorCode, SnipeObject data, Action<string, SnipeObject> handler)
+		{
+			if (_logicTable.Loaded)
+			{
+				handler.Invoke(errorCode, data);
+				return;
+			}
+
+			_waitingTableMessages ??= new Queue<QueuedMessage>();
+			_waitingTableMessages.Enqueue(new QueuedMessage() { messageType = messageType, errorCode = errorCode, data = data });
+		}
+
+		private void OnLogicGet(string errorCode, SnipeObject responseData)
 		{
 			_updateRequestedTime = TimeSpan.Zero; // reset timer  // ????????
-			
-			if (_logicTable == null || response_data == null)
-				return;
 
-			if (error_code != "ok")
-				return;
-
-			var logic_nodes = new List<LogicNode>();
-			if (response_data["logic"] is IList src_logic)
+			if (responseData == null || errorCode != "ok")
 			{
-				foreach (object o in src_logic)
+				return;
+			}
+
+			var logicNodes = new List<LogicNode>();
+			if (responseData["logic"] is IList srcLogic)
+			{
+				foreach (object o in srcLogic)
 				{
 					if (o is SnipeObject so && so?["node"] is SnipeObject node)
 					{
-						logic_nodes.Add(new LogicNode(node, _logicTable));
+						logicNodes.Add(new LogicNode(node, _logicTable));
 					}
 				}
 			}
 				
-			bool timer_finished = false;
+			bool timerFinished = false;
 
 			if (Nodes.Count == 0)
 			{
 				_taggedNodes = new Dictionary<string, LogicNode>();
-				foreach (LogicNode node in logic_nodes)
+				foreach (LogicNode node in logicNodes)
 				{
 					if (node == null)
 						continue;
@@ -236,19 +274,19 @@ namespace MiniIT.Snipe.Api
 			{
 				_taggedNodes.Clear();
 
-				foreach (var node in logic_nodes)
+				foreach (var node in logicNodes)
 				{
 					if (node != null && node.id > 0)
 					{
 						if (node.timeleft == 0) // (-1) means that the node does not have a timer
 						{
-							timer_finished = true;
+							timerFinished = true;
 						}
 
-						if (Nodes.TryGetValue(node.id, out var stored_node))
+						if (Nodes.TryGetValue(node.id, out var storedNode))
 						{
-							stored_node.CopyVars(node);
-							AddTaggedNode(stored_node);
+							storedNode.CopyVars(node);
+							AddTaggedNode(storedNode);
 						}
 						else
 						{
@@ -258,13 +296,13 @@ namespace MiniIT.Snipe.Api
 					}
 				}
 
-				List<int> nodes_to_exclude = null;
-				foreach (var stored_node_id in Nodes.Keys)
+				List<int> nodesToExclude = null;
+				foreach (var storedNodeId in Nodes.Keys)
 				{
 					bool found = false;
-					foreach (var node in logic_nodes)
+					foreach (var node in logicNodes)
 					{
-						if (node != null && node.id == stored_node_id)
+						if (node != null && node.id == storedNodeId)
 						{
 							found = true;
 							break;
@@ -272,14 +310,14 @@ namespace MiniIT.Snipe.Api
 					}
 					if (!found)
 					{
-						nodes_to_exclude ??= new List<int>();
-						nodes_to_exclude.Add(stored_node_id);
+						nodesToExclude ??= new List<int>();
+						nodesToExclude.Add(storedNodeId);
 					}
 				}
 
-				if (nodes_to_exclude != null)
+				if (nodesToExclude != null)
 				{
-					foreach (int key in nodes_to_exclude)
+					foreach (int key in nodesToExclude)
 					{
 						Nodes.Remove(key);
 					}
@@ -289,7 +327,7 @@ namespace MiniIT.Snipe.Api
 			// Highly unlikely but sometimes
 			// messages may contain zero timer values (because of rounding).
 			// In this case just request once more
-			if (timer_finished)
+			if (timerFinished)
 			{
 				RequestLogicGet(true);
 			}
@@ -314,7 +352,7 @@ namespace MiniIT.Snipe.Api
 			}
 		}
 
-		private void OnLogicExitNode(SnipeObject data)
+		private void OnLogicExitNode(string errorCode, SnipeObject data)
 		{
 			LogicNode node = GetNodeById(data.SafeGetValue("id", 0));
 			ExitNode?.Invoke(node, data["results"] as List<object>);
@@ -322,7 +360,7 @@ namespace MiniIT.Snipe.Api
 			RequestLogicGet(true);
 		}
 
-		private void OnLogicProgress(SnipeObject data)
+		private void OnLogicProgress(string errorCode, SnipeObject data)
 		{
 			//Progress?.Invoke(data.SafeGetValue<int>("id"),
 			//			data.SafeGetValue<int>("treeID"),
@@ -354,6 +392,35 @@ namespace MiniIT.Snipe.Api
 
 				int oldValue = data.SafeGetValue<int>("oldValue");
 				NodeProgress?.Invoke(node, nodeVar, oldValue);
+			}
+		}
+
+		private async void WaitForTableLoaded(CancellationToken cancellation)
+		{
+			while (_logicTable != null && !_logicTable.Loaded)
+			{
+				try
+				{
+					await Task.Delay(100, cancellation);
+				}
+				catch (OperationCanceledException)
+				{
+					return;
+				}
+
+				if (cancellation.IsCancellationRequested)
+				{
+					return;
+				}
+			}
+
+			if (_waitingTableMessages != null)
+			{
+				while (_waitingTableMessages.Count > 0)
+				{
+					QueuedMessage message = _waitingTableMessages.Dequeue();
+					OnSnipeMessageReceived(message.messageType, message.errorCode, message.data, 0);
+				}
 			}
 		}
 
