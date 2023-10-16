8f8a79a5acd4eb6712d1e809f032b9468279b83e
gluks <gluk-02@yandex.ru>
2023-10-16 18:54:05 +0600

    TryDequeue wokraround

--- Runtime/Snipe/Communicator/Client/SnipeClient.cs
+++ Runtime/Snipe/Communicator/Client/SnipeClient.cs
@@ -104,8 +104,14 @@ namespace MiniIT.Snipe
 
 		private bool StartNextTransport()
 		{
+#if NET_STANDARD_2_1
 			if (_transportFactoriesQueue.TryDequeue(out var transportFactory))
 			{
+#else
+			if (_transportFactoriesQueue.Count > 0)
+			{
+				var transportFactory = _transportFactoriesQueue.Dequeue();
+#endif
 				StartTransport(transportFactory);
 				return true;
 			}
