ee92367201185e95732f92407187ca8ec954e517
gluks <gluk-02@yandex.ru>
2024-01-19 14:32:27 +0600

    Update BuiltInTablesListService.cs

--- Runtime/Snipe.Unity/Communicator/API/Tables/Internal/BuiltInTablesListService.cs
+++ Runtime/Snipe.Unity/Communicator/API/Tables/Internal/BuiltInTablesListService.cs
@@ -55,7 +55,7 @@ namespace MiniIT.Snipe.Tables
 			string json = await StreamingAssetsReader.ReadTextAsync("snipe_tables.json", cancellationToken);
 			if (string.IsNullOrEmpty(json))
 			{
-				SnipeServices.LogService.GetLogger(nameof(BuiltInTablesListService)).LogError("ReadBuiltInTablesVersions failed to load or parse snipe_tables.json");
+				SnipeServices.LogService.GetLogger(nameof(BuiltInTablesListService)).LogError("ReadBuiltInTablesVersions failed to read snipe_tables.json");
 				return result;
 			}
 
