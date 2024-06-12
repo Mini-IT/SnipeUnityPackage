02e48c99e5259d8fa437086779e1905215573d2f
gluks <gluk-02@yandex.ru>
2024-06-12 18:16:58 +0500

    Update PackageInfo.cs

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -3,6 +3,6 @@ namespace MiniIT.Snipe
 	public static class PackageInfo
 	{
 		public static string VERSION_CODE = "6.1.1";
-		public static string VERSION_NAME = "6.1.1-dev";
+		public static string VERSION_NAME = "6.1.1";
 	}
 }
