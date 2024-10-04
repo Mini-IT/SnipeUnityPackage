b22e954eba7fb6724f304bb1c0d507e24ecdb978
gluks <gluk-02@yandex.ru>
2024-10-04 12:47:44 +0500

    Package version 6.3.0

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -2,7 +2,7 @@ namespace MiniIT.Snipe
 {
 	public static class PackageInfo
 	{
-		public static string VERSION_CODE = "6.2.0";
-		public static string VERSION_NAME = "6.2.0-dev";
+		public static string VERSION_CODE = "6.3.0";
+		public static string VERSION_NAME = "6.3.0";
 	}
 }
