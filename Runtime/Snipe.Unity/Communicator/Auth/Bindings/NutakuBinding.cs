fdc0e73e9a2fea7b0d623259426c5f3354449bd6
SpaceLab322 <53530036+SpaceLab322@users.noreply.github.com>
2024-11-06 16:41:36 +0700

    Nutaku

---
+++ Runtime/Snipe.Unity/Communicator/Auth/Bindings/NutakuBinding.cs
@@ -0,0 +1,12 @@
+﻿#if NUTAKU
+namespace MiniIT.Snipe.Unity
+{
+	public class NutakuBinding : AuthBinding<NutakuIdFetcher>
+	{
+		public NutakuBinding(SnipeCommunicator communicator, AuthSubsystem authSubsystem, SnipeConfig config)
+			: base("dvid", communicator, authSubsystem, config)
+		{
+		}
+	}
+}
+#endif
