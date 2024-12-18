1ab264c96aadf8a9f3c12bbca77430da600b91f2
gluks <gluk-02@yandex.ru>
2024-12-18 17:19:46 +0500

    ISnipeErrorsTracker

--- Runtime/Snipe.Unity/Services/UnitySnipeServicesFactory.cs
+++ Runtime/Snipe.Unity/Services/UnitySnipeServicesFactory.cs
@@ -7,6 +7,10 @@ namespace MiniIT.Snipe.Unity
 {
 	public class UnitySnipeServicesFactory : ISnipeServiceLocatorFactory
 	{
+#if UNITY_EDITOR
+		public static Debugging.ISnipeErrorsTracker DebugErrorsTracker { get; set; }
+#endif
+
 		public virtual ISharedPrefs CreateSharedPrefs() =>
 #if UNITY_WEBGL && !UNITY_EDITOR
 			new WebGLSharedPrefs();
@@ -15,7 +19,14 @@ namespace MiniIT.Snipe.Unity
 #endif
 
 		public virtual ILogService CreateLogService() => new UnityLogService();
-		public virtual ISnipeAnalyticsService CreateAnalyticsService() => new SnipeAnalyticsService();
+
+		public virtual ISnipeAnalyticsService CreateAnalyticsService() =>
+#if UNITY_EDITOR
+			new SnipeAnalyticsService(DebugErrorsTracker);
+#else
+			new SnipeAnalyticsService();
+#endif
+
 		public virtual IMainThreadRunner CreateMainThreadRunner() => new MainThreadRunner();
 		public virtual IApplicationInfo CreateApplicationInfo() => new UnityApplicationInfo();
 		public virtual IStopwatchFactory CreateFuzzyStopwatchFactory() => new FuzzyStopwatchFactory();
