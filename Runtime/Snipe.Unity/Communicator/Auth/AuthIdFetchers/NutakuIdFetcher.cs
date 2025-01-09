7dd85180bf06cf0199085122bf03bf5ea9fb04cc
SpaceLab322 <53530036+SpaceLab322@users.noreply.github.com>
2025-01-09 17:11:22 +0700

    Nutaku

---
+++ Runtime/Snipe.Unity/Communicator/Auth/AuthIdFetchers/NutakuIdFetcher.cs
@@ -0,0 +1,36 @@
+﻿#if NUTAKU
+using System;
+using Nutaku.Unity;
+
+namespace MiniIT.Snipe.Unity
+{
+	public class NutakuIdFetcher : AuthIdFetcher
+	{
+		public override void Fetch(bool wait_initialization, Action<string> callback = null)
+		{
+			if (string.IsNullOrEmpty(Value))
+			{
+				string userId = SdkPlugin.loginInfo?.userId;
+				if (!string.IsNullOrEmpty(userId))
+				{
+					SetValue(userId);
+				}
+			}
+			callback?.Invoke(Value);
+		}
+
+		public void SetValue(string value)
+		{
+			if (CheckValueValid(value))
+				Value = value;
+			else
+				Value = "";
+		}
+
+		private static bool CheckValueValid(string value)
+		{
+			return !string.IsNullOrEmpty(value) && value.ToLower() != "fakeid";
+		}
+	}
+}
+#endif
