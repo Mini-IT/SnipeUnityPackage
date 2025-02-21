11765b31a1165c462ac2edf7ab66e1f0a4767161
gluks <gluk-02@yandex.ru>
2025-02-21 18:13:24 +0500

    Update BaseSnipeApiContext.cs

--- Runtime/Snipe.Unity/Context/BaseSnipeApiContext.cs
+++ Runtime/Snipe.Unity/Context/BaseSnipeApiContext.cs
@@ -3,49 +3,82 @@ using MiniIT.Snipe.Api;
 
 namespace MiniIT.Snipe
 {
-	public class BaseSnipeApiContext : SnipeContext
+	/* Usage example */
+	/*
+	public class SnipeApiContextFactory : AbstractSnipeApiContextFactory, ISnipeApiContextItemsFactory
+	{
+		protected override SnipeContext InternalCreateContext(int id, SnipeConfig config, SnipeCommunicator communicator, AuthSubsystem auth, LogReporter logReporter)
+		{
+			return new SnipeApiContext(id, config, communicator, auth, logReporter, this);
+		}
+
+		public TimeSpan GetServerTimeZoneOffset() => TimeSpan.FromHours(3);
+		public SnipeApiTables CreateSnipeApiTables() => new SnipeTables();
+		public AbstractSnipeApiService CreateSnipeApiService(SnipeCommunicator communicator, AuthSubsystem auth) => new SnipeApiService(communicator, auth);
+	}
+
+	public static class SnipeApiContextExtension
+	{
+		public static SnipeApiService GetApi(this SnipeApiContext context) => (SnipeApiService)context.GetSnipeApiService();
+		public static SnipeTables GetTables(this SnipeApiContext context) => (SnipeTables)context.GetSnipeApiTables();
+	}
+	*/
+
+	public interface ISnipeApiContextItemsFactory
+	{
+		TimeSpan GetServerTimeZoneOffset();
+		SnipeApiTables CreateSnipeApiTables();
+		AbstractSnipeApiService CreateSnipeApiService(SnipeCommunicator communicator, AuthSubsystem auth);
+	}
+
+	public class SnipeApiContext : SnipeContext
 	{
 		public LogicManager LogicManager { get; }
 		public GraphLogicManager GraphManager { get; }
 		public BadgesManager BadgesManager { get; }
 		public CalendarManager CalendarManager { get; }
 
-		protected AbstractSnipeApiService _api;
-		protected SnipeApiTables _tables;
+		private readonly AbstractSnipeApiService _api;
+		private readonly SnipeApiTables _tables;
 
-		public BaseSnipeApiContext(int id, SnipeConfig config, SnipeCommunicator communicator, AuthSubsystem auth, LogReporter logReporter,
-			AbstractSnipeApiService api, SnipeApiTables tables, TimeZoneInfo serverTimeZone)
+		public SnipeApiContext(int id, SnipeConfig config, SnipeCommunicator communicator, AuthSubsystem auth, LogReporter logReporter,
+			ISnipeApiContextItemsFactory itemsFactory)
 			: base(id, config, communicator, auth, logReporter)
 		{
-			_api = api;
-			_tables = tables;
+			_api = itemsFactory.CreateSnipeApiService(communicator, auth);
+			_tables = itemsFactory.CreateSnipeApiTables();
 
-			var logicTable = tables.GetTable<SnipeTableLogicItem>();
+			var logicTable = _tables.GetTable<SnipeTableLogicItem>();
 			if (logicTable != null)
 			{
 				LogicManager = new LogicManager(Communicator, CreateRequest, logicTable);
 			}
 
-			var graphsTable = tables.GetTable<SnipeTableGraphsItem>();
+			var graphsTable = _tables.GetTable<SnipeTableGraphsItem>();
 			if (graphsTable != null)
 			{
 				GraphManager = new GraphLogicManager(Communicator, CreateRequest, graphsTable);
 			}
 
-			var badgesTable = tables.GetTable<SnipeTableBadgesItem>();
+			var badgesTable = _tables.GetTable<SnipeTableBadgesItem>();
 			if (badgesTable != null)
 			{
 				BadgesManager = new BadgesManager(Communicator, CreateRequest, badgesTable);
 			}
 
-			var calendarTable = tables.GetTable<SnipeTableCalendarItem>();
+			var calendarTable = _tables.GetTable<SnipeTableCalendarItem>();
 			if (calendarTable != null)
 			{
+				TimeSpan serverTimeZoneOffset = itemsFactory.GetServerTimeZoneOffset();
+				var serverTimeZone = TimeZoneInfo.CreateCustomTimeZone("server time", serverTimeZoneOffset, "server time", "server time");
 				CalendarManager = new CalendarManager(calendarTable, serverTimeZone);
 			}
 
 			Communicator.ConnectionSucceeded += OnCommunicatorConnected;
 		}
+
+		public AbstractSnipeApiService GetSnipeApiService() => _api;
+		public SnipeApiTables GetSnipeApiTables() => _tables;
 
 		private void OnCommunicatorConnected()
 		{
