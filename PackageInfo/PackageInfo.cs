545b831bd9ebc5bf3829deb4e471e683edae5f4d
gluks <gluk-02@yandex.ru>
2025-03-31 11:38:44 +0500

    Update PackageInfo.cs

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -2,7 +2,7 @@ namespace MiniIT.Snipe
 {
 	public static class PackageInfo
 	{
-		public static string VERSION_CODE = "7.1.0";
-		public static string VERSION_NAME = "7.1.0-dev";
+		public static string VERSION_CODE = "7.1.1";
+		public static string VERSION_NAME = "7.1.1-dev";
 	}
 }
