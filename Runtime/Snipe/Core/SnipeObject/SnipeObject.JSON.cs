28541de8f772522492a4eabebd4f42b7e6deb488
gluks <gluk-02@yandex.ru>
2025-04-01 16:02:47 +0500

    SnipeObject JSON SerializeString

--- Runtime/Snipe/Core/SnipeObject/SnipeObject.JSON.cs
+++ Runtime/Snipe/Core/SnipeObject/SnipeObject.JSON.cs
@@ -70,12 +70,7 @@ namespace MiniIT
 			}
 			else if (obj is string str)
 			{
-				string_builder.Append('"');
-				// escape '\' and '"'
-				str = str.Replace("\\", "\\\\");
-				str = str.Replace("\"", "\\\"");
-				string_builder.Append(str);
-				string_builder.Append('"');
+				SerializeString(string_builder, str);
 			}
 			else if (obj is IEnumerable)
 			{
@@ -109,72 +104,53 @@ namespace MiniIT
 			}
 		}
 
-		/// <summary>
-		/// Escaping spesial characters including escaping of Unicode and ASCII non printable characters
-		/// http://stackoverflow.com/a/14087738
-		/// </summary>
-		/// <returns>String with the special characters escaped</returns>
-		/// <param name="input">string to process</param>
-		/*
-		protected static string ToLiteral(string input)
+		private static void SerializeString(StringBuilder builder, string str)
 		{
-			StringBuilder literal = new StringBuilder (input.Length + 2);
-			literal.Append ("\"");
-			foreach (var c in input)
+			builder.Append('\"');
+
+			ReadOnlySpan<char> charArray = str.AsSpan();
+			foreach (char c in charArray)
 			{
 				switch (c)
 				{
-				case '\'':
-					literal.Append (@"\'");
-					break;
-				case '\"':
-					literal.Append ("\\\"");
-					break;
-				case '\\':
-					literal.Append (@"\\");
-					break;
-				case '\0':
-					literal.Append (@"\0");
-					break;
-				case '\a':
-					literal.Append (@"\a");
-					break;
-				case '\b':
-					literal.Append (@"\b");
-					break;
-				case '\f':
-					literal.Append (@"\f");
-					break;
-				case '\n':
-					literal.Append (@"\n");
-					break;
-				case '\r':
-					literal.Append (@"\r");
-					break;
-				case '\t':
-					literal.Append (@"\t");
-					break;
-				case '\v':
-					literal.Append (@"\v");
-					break;
-				default:
-					// ASCII printable character
-					if (c >= 0x20 && c <= 0x7e)
-					{
-						literal.Append (c);
-						// As UTF16 escaped character
-					} else
-					{
-						literal.Append (@"\u");
-						literal.Append (((int)c).ToString ("x4"));
-					}
-					break;
+					case '"':
+						builder.Append("\\\"");
+						break;
+					case '\\':
+						builder.Append("\\\\");
+						break;
+					case '\b':
+						builder.Append("\\b");
+						break;
+					case '\f':
+						builder.Append("\\f");
+						break;
+					case '\n':
+						builder.Append("\\n");
+						break;
+					case '\r':
+						builder.Append("\\r");
+						break;
+					case '\t':
+						builder.Append("\\t");
+						break;
+					default:
+						var codepoint = Convert.ToInt32(c);
+						if ((codepoint >= 32) && (codepoint <= 126))
+						{
+							builder.Append(c);
+						}
+						else
+						{
+							builder.Append("\\u");
+							builder.Append(codepoint.ToString("x4"));
+						}
+						break;
 				}
 			}
-			literal.Append ("\"");
-			return literal.ToString();
+
+			builder.Append('\"');
 		}
-		*/
 
 		#endregion
 	}
