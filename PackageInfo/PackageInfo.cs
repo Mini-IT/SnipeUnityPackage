599f8c72ff84d7846357928dc894f69c0d6000a1
gluks <gluk-02@yandex.ru>
2025-05-06 16:06:28 +0500

    7.2.0-config-fetch-analytics

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -2,7 +2,7 @@ namespace MiniIT.Snipe
 {
 	public static class PackageInfo
 	{
-		public static string VERSION_CODE = "7.1.8";
-		public static string VERSION_NAME = "7.1.8-config-fetch-analytics";
+		public static string VERSION_CODE = "7.2.0";
+		public static string VERSION_NAME = "7.2.0-config-fetch-analytics";
 	}
 }
