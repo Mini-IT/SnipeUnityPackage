bea8b5ee51216375922ef98ae9afd78b875ec539
gluks <gluk-02@yandex.ru>
2025-05-13 11:18:47 +0500

    Update AuthSubsystem.cs

--- Runtime/Snipe/Communicator/Auth/AuthSubsystem.cs
+++ Runtime/Snipe/Communicator/Auth/AuthSubsystem.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using Microsoft.Extensions.Logging;
