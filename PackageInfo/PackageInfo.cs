0ef961f657780c2aa1c5c76bb6f2c6f75dc49c80
gluks <gluk-02@yandex.ru>
2025-05-15 14:25:24 +0500

    7.2.2-main

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -3,6 +3,6 @@ namespace MiniIT.Snipe
 	public static class PackageInfo
 	{
 		public static string VERSION_CODE = "7.2.2";
-		public static string VERSION_NAME = "7.2.2-dev";
+		public static string VERSION_NAME = "7.2.2-main";
 	}
 }
