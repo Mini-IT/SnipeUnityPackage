68aa6b40c85e0ce9ebb958c0cd4832e780223f0b
gluks <gluk-02@yandex.ru>
2025-06-10 13:50:29 +0200

    Update HttpTransport.cs

--- Runtime/Snipe/Communicator/Client/Transport/Http/HttpTransport.cs
+++ Runtime/Snipe/Communicator/Client/Transport/Http/HttpTransport.cs
@@ -16,7 +16,7 @@ namespace MiniIT.Snipe
 		private const string API_PATH = "api/v1/request/";
 
 		private static readonly SnipeObject s_pingMessage = new SnipeObject() { ["t"] = "server.ping", ["id"] = -1 };
-		private static readonly long s_sessionDurationTicks = TimeSpan.FromSeconds(301).Ticks;
+		private static readonly long s_sessionDurationTicks = 301 * Stopwatch.Frequency;
 
 		public override bool Started => _started;
 		public override bool Connected => _connected;
