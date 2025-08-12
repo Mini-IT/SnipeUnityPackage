459eec676026a51c7d490416fb0f2cfdaadc93fb
gluks <gluk-02@yandex.ru>
2025-08-12 12:20:54 +0200

    Facebook moved to Integrations

--- Runtime/Snipe.Unity/Communicator/Auth/UnityAuthSubsystem.cs
+++ Runtime/Snipe.Unity/Communicator/Auth/UnityAuthSubsystem.cs
@@ -3,36 +3,6 @@ using Cysharp.Threading.Tasks;
 
 namespace MiniIT.Snipe.Unity
 {
-	public static class AuthSubsystemExt
-	{
-		public static void RegisterDefaultBindings(this AuthSubsystem auth)
-		{
-			if (!auth.TryGetBinding<DeviceIdBinding>(false, out _))
-			{
-				auth.RegisterBinding(new DeviceIdBinding());
-			}
-
-			if (!auth.TryGetBinding<AdvertisingIdBinding>(false, out _))
-			{
-				auth.RegisterBinding(new AdvertisingIdBinding());
-			}
-
-#if SNIPE_FACEBOOK
-			if (!auth.TryGetBinding<FacebookBinding>(false, out _))
-			{
-				auth.RegisterBinding(new FacebookBinding());
-			}
-#endif
-
-#if UNITY_ANDROID
-			if (!auth.TryGetBinding<AmazonBinding>(false, out _))
-			{
-				auth.RegisterBinding(new AmazonBinding());
-			}
-#endif
-		}
-	}
-
 	public class UnityAuthSubsystem : AuthSubsystem
 	{
 		public UnityAuthSubsystem(int contextId, SnipeCommunicator communicator, SnipeAnalyticsTracker analytics)
