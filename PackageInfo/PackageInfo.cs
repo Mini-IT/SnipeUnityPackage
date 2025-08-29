4f63ce30fa7870617005a039e3a76fb85e20b26e
gluks <gluk-02@yandex.ru>
2025-08-29 16:47:52 +0200

    7.3.4-main

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -3,6 +3,6 @@ namespace MiniIT.Snipe
 	public static class PackageInfo
 	{
 		public static string VERSION_CODE = "7.3.4";
-		public static string VERSION_NAME = "7.3.4-dev";
+		public static string VERSION_NAME = "7.3.4-main";
 	}
 }
