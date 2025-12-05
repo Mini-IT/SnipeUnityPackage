64340fd52e6b0c63ad7895b87a6a97385d753769
gluks <gluk-02@yandex.ru>
2025-12-05 12:40:47 +0500

    ProfileAttributeExtensions

---
+++ Runtime/Snipe.Unity/Communicator/API/UserAttributes/ProfileManager/ProfileAttributeExtensions.cs
@@ -0,0 +1,10 @@
+namespace MiniIT.Snipe.Api
+{
+	public static class ProfileAttributeExtensions
+	{
+		public static T GetValue<T>(this ProfileAttribute<T> valueSyncer) => valueSyncer.Value;
+		public static void SetValue<T>(this ProfileAttribute<T> valueSyncer, T value) => valueSyncer.Value = value;
+		public static void Increase(this ProfileAttribute<int> valueSyncer, int amount = 1) => valueSyncer.Value += amount;
+		public static void Decrease(this ProfileAttribute<int> valueSyncer, int amount = 1) => valueSyncer.Value -= amount;
+	}
+}
