e525ad0be548f8f63759051c3ec2641bf3c9244c
gluks <gluk-02@yandex.ru>
2026-01-16 13:49:05 +0500

    8.4.4-dev

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -2,7 +2,7 @@ namespace MiniIT.Snipe
 {
 	public static class PackageInfo
 	{
-		public static string VERSION_CODE = "8.4.3";
-		public static string VERSION_NAME = "8.4.3-main";
+		public static string VERSION_CODE = "8.4.4";
+		public static string VERSION_NAME = "8.4.4-dev";
 	}
 }
