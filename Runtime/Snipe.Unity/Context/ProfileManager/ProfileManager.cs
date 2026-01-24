c132f90179f66b0aa399490333b4f7cce5982466
Azev Anatolii <58653966+AzevAnatolii@users.noreply.github.com>
2026-01-24 12:06:21 +0300

    Fix compile error

--- Runtime/Snipe.Unity/Context/ProfileManager/ProfileManager.cs
+++ Runtime/Snipe.Unity/Context/ProfileManager/ProfileManager.cs
@@ -725,12 +725,11 @@ namespace MiniIT.Snipe.Api
 			_snapshotProcessedKeys.Clear();
 			_serverVersionAttrKey = null;
 		}
+		private object NormalizeServerValue(object value) => value switch
+		{
+			byte or sbyte or short or ushort => Convert.ToInt32(value),
+			_ => value
+		};
 	}
 
-	private object NormalizeServerValue(object value) =>
-	value switch
-	{
-		byte or sbyte or short or ushort => Convert.ToInt32(value),
-		_ => value
-	};
 }
