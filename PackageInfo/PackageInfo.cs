bc1149fc81ee5aa62b2906923f3538277df74acb
gluks <gluk-02@yandex.ru>
2026-07-14 13:42:38 +0500

    9.0.9

--- PackageInfo/PackageInfo.cs
+++ PackageInfo/PackageInfo.cs
@@ -2,7 +2,7 @@ namespace MiniIT.Snipe
 {
 	public static class PackageInfo
 	{
-		public static string VERSION_CODE = "9.0.8";
-		public static string VERSION_NAME = "9.0.8-main";
+		public static string VERSION_CODE = "9.0.9";
+		public static string VERSION_NAME = "9.0.9-main";
 	}
 }
